81d0fd3bc338d8bc317fa03b12f3189eab139bdc
ChuanxinKe <kcxwdzx@sina.com>
2020-05-04 10:04:12 +0100

    存在bug,没找到原因,但是目前能用

--- learningcode/get_tweet_sentiment.py
+++ learningcode/get_tweet_sentiment.py
@@ -16,7 +16,7 @@ import TerDec as td #个人开发模块,需要TerDec.py支持
 
 mission_inti=td.Mission('开始初始化')
 
-mission0_1=td.Mission('初始化变量并接连主数据库')
+mission0_1=td.Mission('初始化变量并接连主数据库,目前有bug数据更改在根目录')
 analyzer = SentimentIntensityAnalyzer()
 ct=td.counter(count='      No. of Tweets put into database: ')
 conn = sqlite3.connect('twitter_sentiment.db') #use whichever name you like for your db. just make sure you use the same db name in your subsequent scripts (i.e. Pt2-4)
@@ -25,18 +25,23 @@ c.execute("CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment
 conn.commit()
 mission0_1.end()
 
-mission0_2=td.Mission('接连today备份数据库')
+mission0_2=td.Mission('接连today备份数据库,目前有bug数据更改在根目录')
 conn_today=sqlite3.connect('(today)twitter_sentiment.db')#建议当天跑完后重命名,然后从空表格(model)twitter_sentiment.db复制重命名,做到每日数据有个备份文件
-c_today=conn.cursor()
+c_today=conn_today.cursor()
 c_today.execute("CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL)")
+conn.commit()
 conn_today.commit()
 mission0_2.end()
 
 mission0_3=td.Mission('读取API Keys')
-ckey=""
-csecret=""
-atoken=""
-asecret=""
+#ckey=""
+#csecret=""
+#atoken=""
+#asecret=""
+ckey="MBVMeG1RfsLOYbmIgLCLtpU3k"
+csecret="EGG0oqJkYN9YpuygcNkqEbmPD07R5DBY4UvMx1TT5EsG4L1g92"
+atoken="1252968724790984704-IWFDW6PBDO1OuDJ2HDW5sOlUNq5P5X"
+asecret="siWgdfiJqp76296fYWzjsuVpfHyvBgnbM7sCrjjKcHxrD"
 mission0_3.end()
 
 mission_inti.end()
@@ -53,8 +58,9 @@ class listener(StreamListener): #listener is being declared as a class inheritin
             vs = analyzer.polarity_scores(tweet)
             sentiment = vs['compound']
             c.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)",(time_ms, tweet, sentiment))
-            conn.commit()
+            
             c_today.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)",(time_ms, tweet, sentiment))
+            conn.commit()
             conn_today.commit()
             ct.flush()
 
